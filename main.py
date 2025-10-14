3a20f6b7c1a3c217496796517a8287fc7177ec17
Sahed Saad <sahedsaad2006@gmail.com>
2025-10-13 20:51:12 -0400

    Main scrapping logic for News Aggregator

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+'''
+News12 The Bronx is one of the most reliable sites for local news
+The first site for news web scrapping
+'''
+site_name_news12 = 'News12 The Bronx'
+news12_bronx_url = 'https://bronx.news12.com/category/bronx-news'
+
+response = requests.get(news12_bronx_url)
+
+if response.status_code == 200:
+    print(f'Successfully connected to the site: {site_name_news12}')
+    print('=' * 40)
+else:
+    print(f'Couldnt connect to the site: {site_name_news12}. Check for issues.')
+    exit()
+
+content = response.content
+
+# Organized the HTML content structure of News12 site
+organized_news = BeautifulSoup(content, 'html.parser')
+
+print(f'Local News: {site_name_news12}')
+print('=' * 40)
+
+headlines = organized_news.find_all('div', {'data-testid': 'category-card'})
+
+print(response.status_code)
+
+print(f'Top {len(headlines)} Headlines:')
+
+for i, headline in enumerate(headlines, start=1):
+    headline_tag = headline.find('p', {'class': 'card-title'})
+    headline_text = headline_tag.get_text().strip()
+
+    print(i, headline_text)
