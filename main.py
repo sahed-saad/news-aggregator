5da0490d54fde6b3d9d7e78c4d713d6e05b9cb19
Sahed Saad <sahedsaad2006@gmail.com>
2025-10-18 22:03:48 -0400

    Refactored datetime functionalities into seperate method

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from datetime import datetime
 News12 The Bronx is one of the most reliable sites for local news
 The first site for news web scrapping
 '''
+
+'''
+Display Current Date and Local Time:
+Today's Date: <Month Name> <Date>, <YYYY>
+Local Time: XX:XX XM
+'''
+def time():
+    current_time = datetime.now()
+    display_date = current_time.strftime("%B %d, %Y")
+    local_time = current_time.strftime("%I:%M %p")
+
+    print(f"Today's Date: {display_date}")
+    print(f"Local Time: {local_time}")
+
 site_name_news12 = 'News12 The Bronx'
 news12_bronx_url = 'https://bronx.news12.com/category/bronx-news'
 
@@ -23,19 +37,8 @@ content = response.content
 # Organized the HTML content structure of News12 site
 organized_news = BeautifulSoup(content, 'html.parser')
 
-'''
-Display Current Date and Local Time:
-Today's Date: <Month Name> <Date>, <YYYY>
-Local Time: XX:XX XM
-'''
-
-current_time = datetime.now()
-display_date = current_time.strftime("%B %d, %Y")
-local_time = current_time.strftime("%I:%M %p")
-
 print(f'Local News: {site_name_news12}')
-print(f"Today's Date: {display_date}")
-print(f"Local Time: {local_time}")
+time()
 print('=' * 40)
 
 headlines = organized_news.find_all('div', {'data-testid': 'category-card'})
